530b368affafe266e7c2d7ff2a72589407a7fd54
TomVerpooten <46865422+TomVerpooten@users.noreply.github.com>
2019-10-06 17:12:50 +0200

    Add files via upload

---
+++ Arrays huiswerk oefeningen Af/Arrays huiswerk oefeningen/Fruit.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Arrays_huiswerk_oefeningen
+{
+    public partial class frmFruit : Form
+    {
+        public frmFruit()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            string strInput;
+
+            strInput = txtInput.Text;
+
+            lbxFruit.Items.Add(strInput);
+            lbxSortedFruit.Items.Add(strInput);
+
+            txtInput.Text = string.Empty;
+        }
+
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            lbxSortedFruit.Show();
+
+            lbxSortedFruit.Sorted = true;
+        }
+    }
+}
---
+++ Arrays huiswerk oefeningen Af/Arrays huiswerk oefeningen/Random 7 Numbers.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Arrays_huiswerk_oefeningen
+{
+    public partial class frmRandom7Numbers : Form
+    {
+        List<int> arrRandomNumbers = new List<int>();
+
+        public frmRandom7Numbers()
+        {
+            InitializeComponent();
+        }
+
+        private void frmRandom7Numbers_Load(object sender, EventArgs e)
+        {
+            int intOutput, intLimit, intMaxValue;
+
+            Random rdNumber = new Random();
+
+            for (intLimit = 0; intLimit < 7; intLimit++)
+            {
+                intOutput = rdNumber.Next(0, 11);
+                lbxList.Items.Add(intOutput);
+            }
+
+            arrRandomNumbers.Add(Convert.ToInt32(lbxList.Text));
+
+            intMaxValue = arrRandomNumbers.Max();
+
+            txtIndex.Text = arrRandomNumbers.ToList().IndexOf(intMaxValue).ToString();            
+
+            txtLength.Text = intLimit.ToString();
+        }
+    }
+}
